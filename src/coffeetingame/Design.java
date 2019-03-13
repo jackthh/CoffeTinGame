800b9bd70b92a4b9574ca4f2f6f39baa5061fc96
jackthh <jack.truonghoanghiep@gmail.com>
2019-03-13 12:06:03 +0700

    Design file

---
+++ src/coffeetingame/Design.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CoffeeTinGame;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Design {
+    /**
+     * Determines the last beans
+     * @require tin not equal null/\ tin.length >=0
+     *@modifies tin
+     * @effect <pre>
+     * while there are at least 2 beans in the tin
+     * take out any two beans in the tin
+     * update the tin
+     * return the last bean.
+     * </pre>
+     * 
+     * @param tin
+     * @return an new array /\ 2 elements of this array are index of two bean in tin
+     * 
+     */
+    // PSEUDOCODE
+    // intialize a new array : arr/\arr.length=2
+ 
+   
+    //  //p=p0 /\t>0
+    // p0 is the initial green been parity
+    // t is sum of GREEN beans and BLUE beans or t= mB+nG
+    // 
+    //      choose the number from tin
+    //      random bean1/\ bean1 is an integer number/\ value bean1 is from 0 to tin.length-1
+    //      random bean2/\ bean2 is an integer number/\ value bean2 is from 0 to tin.length-1
+    //      if bi1 is equal bi2 \/ tin[bi1]=REMOVED \/tin[bi2]=REMOVED
+   
+    
+    //return arr[];
+    //
+}
